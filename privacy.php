b0fc0c2d4c22cb05929132c6fbb2640b62e134c8
Mansilla Derqui <IGNACIOM@iadb.org>
2017-06-29 17:31:50 -0300

    29-6 17.31

---
+++ privacy.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Your privacy matter | CODICI Labs</title>
+    <link rel="icon" href="img/logo-BN.ico" type="image/x-icon">
+
+    <!-- css -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- google fonts -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:200" rel="stylesheet"> -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> -->
+
+</head>
+
+<body>
+	<header class="col-xs-12">
+		<div class="container-fluid">
+			<div class="col-xs-12 title-privacy">
+				<div class="logo-title-privacy">
+					<img src="./img/logo-blanco.png">
+				</div>
+				<div>
+					<h1>Privacy Policy</h1>
+				</div>
+			</div>
+		</div>
+	</header>
+		<div class="container">
+			<div class="body-body-privacy">Protecting your private information is our priority. This Statement of Privacy applies to the www.codicilabs.com.ar and Codicilabs and clients data collection and usage. For the purposes of this Privacy Policy, unless otherwise noted, all references to Sonikpass include www.codicilabs.com.ar and Codicilabs. The Codicilabs website is a product marketing site. </div><br>
+
+			<div class="body-title-privacy">Collection of your Personal Information</div>
+
+			<div class="body-body-privacy">Codicilabs may collect personally identifiable information, such as your name. We may gather additional personal or non-personal information in the future.
+			Information about your computer hardware and software may be automatically collected by Codicilabs. This information can include: your IP address, browser type, domain names, access times and referring website addresses. This information is used for the operation of the service, to maintain quality of the service, and to provide general statistics regarding use of the Codicilabs website. Codicilabs encourages you to review the privacy statements of websites you choose to link to from Codicilabs so that you can understand how those websites collect, use and share your information. Codicilabs is not responsible for the privacy statements or other content on websites outside of the Codicilabs website.</div><br>
+
+			<div class="body-title-privacy">Use of your Personal Information</div>
+
+			<div class="body-body-privacy">Sonikpass collects and uses your personal information to operate its website(s) and deliver the services you have requested. 
+			Sonikpass may also use your personally identifiable information to inform you of other products or services available from Sonikpass and its affiliates. Sonikpass may also contact you via surveys to conduct research about your opinion of current services or of potential new services that may be offered. 
+			Sonikpass does not sell, rent or lease its customer lists to third parties. 
+			Sonikpass may share data with trusted partners to help perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to Sonikpass, and they are required to maintain the confidentiality of your information. 
+			Sonikpass will disclose your personal information, without notice, only if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on Sonikpass or the site; (b) protect and defend the rights or property of Sonikpass; and, (c) act under exigent circumstances to protect the personal safety of users of Sonikpass, or the public.</div><br>
+
+			<div class="body-title-privacy">Security of your Personal Information</div>
+ 
+ 			<div class="body-body-privacy">Codicilabs secures your personal information from unauthorized access, use or disclosure.</div><br>
+
+ 			<div class="body-title-privacy">Opt-Out & Unsubscribe</div>
+
+ 			<div class="body-body-privacy">We respect your privacy and give you an opportunity to opt-out of receiving announcements of certain information. Users may opt-out of receiving any or all communications from Codicilabs by contacting us here:<br>
+
+			Web page: www.sonikpass.com
+			Email: info@sonikpass.com
+			Phone: (650) 567-6645
+			</div><br>
+
+			<div class="body-title-privacy">Changes to this Statement</div>
+
+			<div class="body-body-privacy">Codicilabs will occasionally update this Statement of Privacy to reflect company and customer feedback. Codicilabs encourages you to periodically review this Statement to be informed of how Codicilabs is protecting your information.<br></div>
+
+			<div class="body-title-privacy">Contact Information</div>
+
+			<div class="body-body-privacy">Codicilabs welcomes your questions or comments regarding this Statement of Privacy. If you believe that Codicilabs has not adhered to this Statement, please contact Codicilabs by: email: info@sonikpass.com or phone at: (650) 567-6645<br></div>
+
+			<div class="body-body-privacy">Effective as of June 10, 2017<br></div>
+		</div>
+</body>
+</html>
+
+
+
